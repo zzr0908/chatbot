e91fb64027fc2e40496324707dcddb7ae6c15f8e
ray <kobeinmemory@163.com>
2024-06-22 19:45:03 +0800

    1

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export WECHATY_LOG="verbose"
 export WECHATY_PUPPET="wechaty-puppet-padlocal"
 export WECHATY_PUPPET_PADLOCAL_TOKEN="puppet_padlocal_79e4a23ce72c4f58b88465d1c1f80d32"
-export WECHATY_PUPPET_SERVER_PORT="22"
+export WECHATY_PUPPET_SERVER_PORT="3389"
 export WECHATY_TOKEN="b750b55b-e5d7-4528-910e-40c97b70fe94"
 sed -i s/token=.*/token=$WECHATY_TOKEN/g .env
 
@@ -15,4 +15,3 @@ docker run -ti \
   -e WECHATY_TOKEN \
   -p "$WECHATY_PUPPET_SERVER_PORT:$WECHATY_PUPPET_SERVER_PORT" \
   wechaty/wechaty:0.65
-—
