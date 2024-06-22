6394b358a571c96c37cfd0806d756b8c97382390
ray <kobeinmemory@163.com>
2024-06-22 20:50:02 +0800

    1

--- chatbot.py
+++ chatbot.py
@@ -1,17 +1,32 @@
 import asyncio, os
-from wechaty import Wechaty
+from typing import List, Optional, Union
+
+from wechaty_puppet import FileBox  # type: ignore
+
+from wechaty import Wechaty, Contact
+from wechaty.user import Message, Room
 
 
 class MyBot(Wechaty):
-    async def on_message(self, msg):
-        from_contact = msg.talker()
+
+    async def on_message(self, msg: Message):
+        """
+        listen for message event
+        """
+        from_contact: Optional[Contact] = msg.talker()
         text = msg.text()
-        room = msg.room()
+        room: Optional[Room] = msg.room()
         if text == 'ding':
-            conversation = from_contact if room is None else room
+            conversation: Union[
+                Room, Contact] = from_contact if room is None else room
             await conversation.ready()
             await conversation.say('dong')
+            file_box = FileBox.from_url(
+                'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/'
+                'u=1116676390,2305043183&fm=26&gp=0.jpg',
+                name='ding-dong.jpg')
+            await conversation.say(file_box)
 
-
-os.environ['TOKEN'] = "b750b55b-e5d7-4528-910e-40c97b70fe94"
-bot = MyBot()
+os.environ['TOKEN'] = "1fe5f846-3cfb-401d-b20c-XXXXX"
+os.environ['WECHATY_PUPPET_SERVICE_ENDPOINT'] = "127.0.0.1:9001"
+asyncio.run(MyBot().start())
